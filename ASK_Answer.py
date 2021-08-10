edc79c9b6f2322fce359938eb8ef470e40e96f9e
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2021-08-10 23:06:24 +0900

    Update ASK_Answer.py

--- ASK_Answer.py
+++ ASK_Answer.py
@@ -1,5 +1,16 @@
-#개발자 wau2380(박이정)
+'''
 
+ASK_AutoAnswer(KR_School) - Python
+Copyright 2021 yijung park(wau2380) All Rights Reserved.
+
+https://github.com/wau2380
+
+본 프로그램의 사용으로 발생한 문제에 대해서는 책임을 지지 않습니다.
+
+위에 규칙에 동의하신다면, False를 True 로 변경해 주세요.
+
+Agree = False
+'''
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
@@ -12,17 +23,17 @@ driver = webdriver.Chrome('')
 URL="https://asked.kr/"
 driver.get(URL)
 
-def ask_login():
-    ID = '#'
-    PassWord = '#'
-    login = driver.find_element_by_name("id") # 로그인 
+def ask_login(): #로그인
+    ID = '#' #아이디를 입력해주세요
+    PassWord = '#' #패스워드를 입력해 주세요
+    login = driver.find_element_by_name("id") 
     login.send_keys(ID)
     login = driver.find_element_by_name("pw")
     login.send_keys(PassWord) 
-    login.send_keys(Keys.ENTER) #로그인
+    login.send_keys(Keys.ENTER) 
 def ask_new_question(): #질문 모아오기
-    new_ask_number= driver.find_element_by_id("new_count")#새질문 갯수 구하기  
-    counts = int(new_ask_number.text) #정수로 전환
+    new_ask_number= driver.find_element_by_id("new_count")
+    counts = int(new_ask_number.text) 
     return counts
 def ask_new(): #새질문 클릭
     elem = driver.find_elements_by_class_name("a_child")[1]
@@ -42,19 +53,19 @@ answer_contents=[]
 
 try:
     ask_login()
-    time.sleep(1) # 한턴 쉬고
+    time.sleep(1)
     ask_new()
     Count=ask_new_question()
 
     while Count!=0:
         for i in range(0,Count+1): #정수로 변환
             answer = driver.find_elements_by_class_name("card_ask")[0]#질문 불러오기
-            answer_contents.append(answer.text)#문자열 전환
+            answer_contents.append(answer.text) #문자열 전환
             
             if any( '키워드' in s for s in answer_contents): #특정 키워드 겹치는 질문 구별하기
                 elem = driver.find_elements_by_class_name("ask_answer_btn")[0].send_keys(Keys.ENTER)
-                elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'input_answer'))) #요소보일때까지 기다리기
-                elem = driver.find_elements_by_class_name("input_answer")[0].send_keys('대답')#대답창 
+                elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'input_answer'))) 
+                elem = driver.find_elements_by_class_name("input_answer")[0].send_keys('대답') 
                 elem = driver.find_elements_by_class_name("ask_asnwer_bottom_btn")[0]
                 Sleep_05()
                 elem.send_keys(Keys.ENTER)
